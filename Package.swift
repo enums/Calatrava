0ce9a2b7dce85f857e8e0a5c06e653c145424e4b
zhengyuqi <zhengyuqi@baidu.com>
2017-12-18 00:04:40 +0800

    update to Perfect 3.x

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
         Target(name: "Calatrava", dependencies:[]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3, minor: 0),
         .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
     ]

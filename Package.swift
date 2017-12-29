d7cae716737ad5d6b82f085b68ac9107de002424
zhengyuqi <zhengyuqi@baidu.com>
2017-12-29 19:45:42 +0800

    【2.0】使用新版 Pjango 1.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3, minor: 0),
-        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/enums/Pjango-MySQL.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
     ]
 )

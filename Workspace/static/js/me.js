4db8a164e64d89255a50e98e41c621fdb4d16784
zhengyuqi <zhengyuqi@baidu.com>
2017-12-31 22:27:28 +0800

    【3.1】新增 Cookie 保存昵称和邮箱

--- Workspace/static/js/me.js
+++ Workspace/static/js/me.js
@@ -129,3 +129,20 @@ function _setReferFloor(floor) {
 	location = "#comment";
 }
 
+function setCookie(cname,cvalue)
+{
+  	document.cookie = cname + "=" + cvalue + ";";
+}
+ 
+function getCookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) 
+	{
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+	}
+	return "";
+}
+

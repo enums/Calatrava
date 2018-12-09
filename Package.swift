3bbc3deb05da04e1a8256ba75871cb0c1d6d22de
Enum <enum@enumsblog.com>
2018-12-09 22:15:05 +0800

    【5.0.1】修复 Linux 下处理依赖时可能出现无法继续的问题

--- Package.swift
+++ Package.swift
@@ -11,18 +11,17 @@ let package = Package(
             targets: ["Calatrava"]),
         ],
     dependencies: [
-        .package(url:"https://github.com/PerfectlySoft/Perfect-HTTPServer.git" , from: "3.0.19"),
-        .package(url:"https://github.com/PerfectlySoft/Perfect-Mustache.git" , from: "3.0.2"),
-        .package(url:"https://github.com/PerfectlySoft/Perfect-CURL.git" , from: "3.1.0"),
-        .package(url:"https://github.com/PerfectlySoft/Perfect-Crypto.git" , from: "3.1.2"),
         .package(url:"https://github.com/enums/Pjango.git" , from: "2.0.0"),
         .package(url:"https://github.com/enums/Pjango-MySQL.git" , from: "2.0.0"),
         .package(url:"https://github.com/enums/Pjango-Postman.git" , from: "2.0.0"),
-        .package(url:"https://github.com/enums/Pjango-SwiftyJSON.git" , from: "1.0.0"),
         ],
     targets: [
         .target(
             name: "Calatrava",
-            dependencies: ["PerfectHTTPServer", "PerfectMustache", "PerfectCURL", "PerfectCrypto", "Pjango", "PjangoMySQL", "PjangoPostman", "SwiftyJSON"])
+            dependencies: [
+                "Pjango",
+                "PjangoMySQL",
+                "PjangoPostman",
+            ])
     ]
 )

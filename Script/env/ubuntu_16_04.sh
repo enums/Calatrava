e1254453cf2203bc2b9c5804a072fd0de39c2847
zhengyuqi <zhengyuqi@baidu.com>
2018-12-10 17:40:27 +0800

    【5.1.1】新增ubuntu16.04环境安装脚本

---
+++ Script/env/ubuntu_16_04.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install git
+git clone https://github.com/enums/Perfect-Ubuntu.git
+cd Perfect-Ubuntu/
+sudo ./install.sh --sure
+sudo apt install -y mysql-client-core-5.7 mysql-client-5.7 mysql-server-core-5.7 mysql-server-core-5.7 mysql-server-5.7 
+echo "done"
